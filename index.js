c47e5d08459aab1d2d47550beeb6eaddcb2e5e1a
Krunal6555 <68369944+Krunal6555@users.noreply.github.com>
2021-07-15 16:57:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import AppProject from './AppProject';
+
+import AppProject from './AppProject' 
+
+ReactDOM.render(
+  <>
+  <AppProject />
+  </>,
+  document.getElementById('root')
+)
+
